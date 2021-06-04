ae9af92d77af488aefd582e368f1f054cda39e0d
drew <andresalazar138@gmail.com>
2021-06-04 17:18:04 -0500

    added grpc methods

--- main.go
+++ main.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
 
 	"github.com/drew138/text-recognition/equations"
+	"github.com/drew138/text-recognition/solver"
+	"google.golang.org/grpc"
 )
 
 func main() {
+
+	l, err := net.Listen("tcp", ":8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := equations
+	gs := grpc.NewServer()
+
+	equations.RegisterEquationServiceServer(gs, &s)
+
+	if err := gs.Serve(l); err != nil {
+		log.Fatal("Failed to serve gRPC server")
+	}
 
 	Ab := [][]float64{
 		{6, -2, 2, 4, 12},
@@ -15,7 +33,7 @@ func main() {
 		{-6, 4, 1, -18, -38},
 	}
 
-	sol, err := equations.SolveSystem(Ab)
+	sol, err := solver.SolveSystem(Ab)
 	if err != nil {
 		fmt.Println(err)
 		return
